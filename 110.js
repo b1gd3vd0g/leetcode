96a71c132756993380798ed2f7f3cb78282a65c2
b1gd3vd0g <devin@bigdevdog.com>
2025-03-28 10:59:25 -0700

    #110 Balanced Binary Tree (Easy)

---
+++ 110.js
@@ -0,0 +1,22 @@
+const isBalanced = (root) => {
+    /** A recursive function to find the height of a balanced tree, or -1 for an unbalanced tree. */
+    const treeHeight = (tree) => {
+        // Base case: tree is null.
+        if (tree === null) {
+            return 0;
+        } else {
+            // tree must be a node.
+            const leftHeight = treeHeight(tree.left);
+            if (leftHeight === -1) {
+                return -1;
+            }
+            const rightHeight = treeHeight(tree.right);
+            if (rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            }
+            return 1 + Math.max(rightHeight, leftHeight);
+        }
+    };
+
+    return treeHeight(root) !== -1;
+};
